18a32531b618939736401d5c4673a328bb3e6660
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-05-22 17:58:52 +0900

    [Bronze III] Title: 직각삼각형, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,13 @@
+while(True):
+  a, b, c = map(int, input().split())
+  if(a == 0 and b == 0 and c == 0):
+    break
+
+  if(a * a + b * b == c * c):
+    print("right")
+  elif(a * a + c * c == b * b):
+    print("right")
+  elif(b * b + c * c == a * a):
+    print("right")
+  else:
+    print("wrong")
