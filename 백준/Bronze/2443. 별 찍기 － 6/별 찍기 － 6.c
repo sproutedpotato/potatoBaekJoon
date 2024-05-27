d077eb1d65f0f4e22b38cf23bbd12eb186ca4b0b
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-05-27 23:38:23 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {  
+   int a;
+   scanf("%d", &a);
+
+   for(int i = 0; i < a; i++){
+      for(int j = 0; j < i; j++){
+         printf(" ");
+      }
+
+      for(int k = 0; k < 2 * (a - i) - 1; k++){
+         printf("*");
+      }
+      printf("\n");
+   }
+}
