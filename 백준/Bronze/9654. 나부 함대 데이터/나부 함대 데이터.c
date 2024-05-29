a5b147828f6e66dfa0f8dd5d0ebedf4f3988af38
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-05-29 23:58:39 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {  
+   printf("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n"
+      "N2 Bomber      Heavy Fighter  Limited    21        \n"
+      "J-Type 327     Light Combat   Unlimited  1         \n"
+      "NX Cruiser     Medium Fighter Limited    18        \n"
+      "N1 Starfighter Medium Fighter Unlimited  25        \n"
+      "Royal Cruiser  Light Combat   Limited    4         ");
+   
+   return 0;
+}
