9af33c9961c861194ed3b4b9f2a586000f394465
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-05-31 23:47:12 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) { 
+   char name[21];
+   scanf("%s", name);
+   printf(":fan::fan::fan:\n");
+   printf(":fan::%s::fan:\n", name);
+   printf(":fan::fan::fan:");
+
+   return 0;
+}
