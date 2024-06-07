a5011162f6505239c52ed2f7317ebc40f6c8544b
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-06-07 23:31:18 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+letter = input()
+
+if(letter == "NLCS"):
+  print("North London Collegiate School")
+elif(letter == "BHA"):
+  print("Branksome Hall Asia")
+elif(letter == "KIS"):
+  print("Korea International School")
+else:
+  print("St. Johnsbury Academy")
