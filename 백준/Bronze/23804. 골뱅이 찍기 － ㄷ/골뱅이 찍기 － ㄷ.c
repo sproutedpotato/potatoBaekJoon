f93d8bce161e827ee05e78112947827ec81465dd
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-06-07 23:45:29 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) { 
+   int a;
+   scanf("%d", &a);
+
+   for(int i = 0; i < a; i++){
+      for(int j = 0; j < a * 5; j++){
+         printf("@");
+      }
+      printf("\n");
+   }
+   
+   for(int i = 0; i < a * 5 - a * 2; i++){
+      for(int j = 0; j < a; j++){
+         printf("@");
+      }
+      printf("\n");
+   }
+
+   for(int i = 0; i < a; i++){
+      for(int j = 0; j < a * 5; j++){
+         printf("@");
+      }
+      printf("\n");
+   }
+   
+   
+   return 0;
+}
