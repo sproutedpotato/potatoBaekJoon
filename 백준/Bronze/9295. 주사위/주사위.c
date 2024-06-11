7004dd4af6eba6a7d76175a23deec6fbfd4c2807
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-06-11 22:19:22 +0900

    [Bronze III] Title: 주사위, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) { 
+   int a, b, c;
+   scanf("%d", &a);
+
+   for(int i = 0; i < a; i++){
+      scanf("%d %d", &b, &c);
+      printf("Case %d: %d\n", i + 1, b + c);
+   }
+
+   return 0;
+}
