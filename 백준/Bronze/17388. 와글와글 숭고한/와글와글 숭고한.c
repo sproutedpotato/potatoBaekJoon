363cd63e1566a7947aaf89613dea78c2983c4c20
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-06-18 23:46:24 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) { 
+   int a, b, c;
+   scanf("%d %d %d", &a, &b, &c);
+   
+   if(a + b + c >= 100){
+      printf("OK");
+   }
+   else{
+      if(a < b && a < c){
+         printf("Soongsil");
+      }
+      else if(b < a && b < c){
+         printf("Korea");
+      }
+      else{
+         printf("Hanyang");
+      }
+   }
+   
+   return 0;
+}
