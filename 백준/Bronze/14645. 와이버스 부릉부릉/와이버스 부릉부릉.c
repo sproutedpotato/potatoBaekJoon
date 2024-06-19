2b6c0f6a6ba7d79d33082a84914c73250e564789
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-06-19 23:45:05 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) { 
+   int a, b, c, d;
+   scanf("%d %d", &a, &b);
+
+   for(int i = 0; i < a; i++){
+      scanf("%d %d", &c, &d);
+   }
+
+   printf("비와이");
+   
+   return 0;
+}
