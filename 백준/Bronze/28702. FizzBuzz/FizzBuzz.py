91415ae7ae2d0e9042ad7937a8ef765a45774e36
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-07-16 21:37:49 +0900

    [Bronze I] Title: FizzBuzz, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.py
@@ -0,0 +1,19 @@
+a = input()
+b = input()
+c = input()
+
+if a.isdigit():
+  result = int(a) + 3
+elif b.isdigit():
+  result = int(b) + 2
+elif c.isdigit():
+  result = int(c) + 1
+
+if result % 3 == 0 and result % 5 == 0:
+  print("FizzBuzz")
+elif result % 3 == 0:
+  print("Fizz")
+elif result % 5 == 0:
+  print("Buzz")
+else:
+  print(result)
