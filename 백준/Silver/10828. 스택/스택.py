504e76abcbebf2933cc4c4210f54d288b07aa049
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-07-22 21:38:27 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,32 @@
+import sys
+
+a = int(sys.stdin.readline())
+
+arr = []
+
+for _ in range(a):
+  ip = sys.stdin.readline()
+  ip = ip[0:-1]
+  if ip[0:4] == "push":
+    arr.append(ip[5:])
+
+  elif ip[0:5] == "empty":
+    if len(arr) == 0:
+      print(1)
+    else:
+      print(0)
+
+  elif ip[0:3] == "pop":
+    if len(arr) == 0:
+      print(-1)
+    else:
+      print(arr.pop())
+
+  elif ip[0:3] == "top":
+    if len(arr) == 0:
+      print(-1)
+    else:
+      print(arr[-1])
+
+  else:
+    print(len(arr))
