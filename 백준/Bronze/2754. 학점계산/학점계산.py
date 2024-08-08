3d8ff0deba6d34fccb3bb249639ef0309acc4569
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-08-08 23:41:57 +0900

    [Bronze III] Title: 학점계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+arr = input()
+
+if(arr == "A+"):
+    print("4.3")
+elif(arr == "A0"):
+    print("4.0")
+elif(arr == "A-"):
+    print("3.7")
+elif(arr == "B+"):
+    print("3.3")
+elif(arr == "B0"):
+    print("3.0")
+elif(arr == "B-"):
+    print("2.7")
+elif(arr == "C+"):
+    print("2.3")
+elif(arr == "C0"):
+    print("2.0")
+elif(arr == "C-"):
+    print("1.7")
+elif(arr == "D+"):
+    print("1.3")
+elif(arr == "D0"):
+    print("1.0")
+elif(arr == "D-"):
+    print("0.7")
+else:
+    print("0.0")
