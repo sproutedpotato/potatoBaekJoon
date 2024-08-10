1f2cd0673d2502fcd323c881ab0fea5c5b984c17
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-08-10 20:46:23 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 64 ms, Memory: 5492 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    var input = Console.ReadLine();
+    int ip = int.Parse(input) - 543;
+          
+    Console.WriteLine("{0}", ip);
+  }
+}
