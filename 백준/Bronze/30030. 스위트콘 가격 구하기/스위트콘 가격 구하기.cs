6fef4feabc077bf4702cfd45c93a5d521b9ab7f3
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-08-13 00:47:19 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    var a = Console.ReadLine();
+    int count = int.Parse(a);
+    
+    Console.WriteLine("{0}", count * 10/11);
+    
+  }
+}
