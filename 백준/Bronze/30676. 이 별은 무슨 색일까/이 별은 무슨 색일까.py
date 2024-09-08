e6029df2eb71188e3906dc694ba108134e634264
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-09-09 00:09:39 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,16 @@
+lamda = int(input())
+
+if 620 <= lamda <= 780:
+  print("Red")
+elif 590 <= lamda < 620:
+  print("Orange")
+elif 570 <= lamda < 590:
+  print("Yellow")
+elif 495 <= lamda < 570:
+  print("Green")
+elif 450 <= lamda < 495:
+  print("Blue")
+elif 425 <= lamda < 450:
+  print("Indigo")
+else:
+  print("Violet")
