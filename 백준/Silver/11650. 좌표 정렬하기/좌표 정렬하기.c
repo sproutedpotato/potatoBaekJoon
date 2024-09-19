f04e799a2264b9ea383904e01b08679da68ea501
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2024-09-19 23:43:01 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 56 ms, Memory: 2556 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b){
+   int *coorA = (int *)a;
+   int *coorB = (int *)b;
+
+   if(coorA[0] == coorB[0])   return coorA[1] - coorB[1];
+   else   return coorA[0] - coorB[0];
+}
+
+int main(void) { 
+   int a;
+   scanf("%d", &a);
+
+   int coor[a][2];
+   for(int i = 0; i < a; i++){
+      scanf("%d %d", &coor[i][0], &coor[i][1]);
+   }
+
+   qsort(coor, a, sizeof(int) * 2, compare);
+
+   for(int i = 0; i < a; i++){
+      printf("%d %d\n", coor[i][0], coor[i][1]);
+   }
+   
+   return 0;
+}
