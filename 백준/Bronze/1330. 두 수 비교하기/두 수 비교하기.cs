e16f3f4df43025dc8049dca12b81a79301a533ec
sproutedpotato <168921417+sproutedpotato@users.noreply.github.com>
2025-03-12 23:53:49 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Baekjoon {
+    class Program {
+        static void Main() {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int a = int.Parse(ss[0]);
+            int b = int.Parse(ss[1]);
+            if(a > b){
+                Console.WriteLine(">");
+            }
+            else if(a < b){
+                Console.WriteLine("<");
+            }
+            else{
+                Console.WriteLine("==");
+            }
+        }
+    }
+  }
